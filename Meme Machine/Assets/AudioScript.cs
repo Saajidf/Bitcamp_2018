36e2182543b15c213a4613532697a0b53cc8ced8
Saajid Farooq <saajidf@gmail.com>
2018-04-08 01:58:54 -0400

    Added existing repos

---
+++ Meme Machine/Assets/AudioScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour {
+
+
+	// Your audio clip
+	public AudioClip MusicClip;
+
+	// the component that Unity uses to play your clip
+	public AudioSource MusicSource;
+
+	// Use this for initialization
+	void Start () {
+		MusicSource.clip = MusicClip;
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+			MusicSource.Play();	
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-Bitcamp_2018
+# Leap-Motion
+Leap Motion Hackathon Bitcamp 2018
+by Pranav Maniktala,
+Saajid Farooq,
+Sepehr Hajbarat,
+ and Will Boodon
